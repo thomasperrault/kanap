50cfcbfaf1116519e5dd57a93101cd3a09b3cc3a
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-08-13 15:10:40 +0200

    Ajout modification de quantité sur l'input

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,17 +2,17 @@
 //Récupération des données du local storage
 let cart = window.localStorage.getItem("product");
 //Modification du format en JSON
-let cartArray= JSON.parse(cart);
-console.log(cartArray);
+let productInLocalStorage= JSON.parse(cart);
+console.log(productInLocalStorage);
 
 /*CART*/
 //Déclaration cartItems issue du HTML
 let cartItems = document.getElementById('cart__items');
 
-//Déclaration de la fonction getCards avec prise en compte des conditions
+//Déclaration de la fonction getcarts avec prise en compte des conditions
 function getCart(){
     //Si le localstorage est vide
-    if (cartArray === null || cartArray == 0){
+    if (productInLocalStorage === null || productInLocalStorage == 0){
         console.log("Panier vide")
         let html="";
         //Utilisation de Ecma script ES6 pour intégration des données dans le HTML
@@ -27,7 +27,7 @@ function getCart(){
     //Si le localstorage n'est pas vide
     else {
         //Récupération des id, colors et quantités pour mise en place de la fonction de doublon
-        for (let infoCart of cartArray){
+        for (let infoCart of productInLocalStorage){
             let infoId = infoCart.infoIdProduct;
             console.log (infoId);
             let infoColor = infoCart.infoColorProduct;
@@ -36,25 +36,25 @@ function getCart(){
             console.log (infoQuantity);
         }
 
-        //Déclaration de la fonction makeCards pour intégration du code HTML
-        function makeCarts(cartArray){
+        //Déclaration de la fonction makecarts pour intégration du code HTML
+        function makeCarts(productInLocalStorage){
             let html="";
-            for(let i=0; i<cartArray.length; i++){
+            for(let i=0; i<productInLocalStorage.length; i++){
                 html = `
-                    <article class="cart__item" data-id="${cartArray[i].infoIdProduct}" data-color="{product-color}">
+                    <article class="cart__item" data-id="${productInLocalStorage[i].infoIdProduct}" data-color="${productInLocalStorage[i].infoColorProduct}">
                         <div class="cart__item__img">
-                            <img src=${cartArray[i].infoImgProduct} alt=${cartArray[i].infoAltImgProduct}>
+                            <img src=${productInLocalStorage[i].infoImgProduct} alt=${productInLocalStorage[i].infoAltImgProduct}>
                         </div>
                         <div class="cart__item__content">
                             <div class="cart__item__content__description">
-                                <h2>${cartArray[i].infoNameProduct}</h2>
-                                <p>${cartArray[i].infoColorProduct}</p>
-                                <p>${cartArray[i].infoPriceProduct} €</p>
+                                <h2>${productInLocalStorage[i].infoNameProduct}</h2>
+                                <p>${productInLocalStorage[i].infoColorProduct}</p>
+                                <p>${productInLocalStorage[i].infoPriceProduct} €</p>
                             </div>
                         <div class="cart__item__content__settings">
                             <div class="cart__item__content__settings__quantity">
                                 <p>Qté :</p>
-                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartArray[i].infoQuantityProduct}">
+                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productInLocalStorage[i].infoQuantityProduct}">
                             </div>
                             <div class="cart__item__content__settings__delete">
                                 <p class="deleteItem">Supprimer</p>
@@ -67,11 +67,11 @@ function getCart(){
             cartItems.insertAdjacentHTML("beforeend", html);
             }
         }
-        //Appel de la fonction makeCards
-        makeCarts(cartArray);
+        //Appel de la fonction makecarts
+        makeCarts(productInLocalStorage);
     }
 }
-//Appel de la fonction getCard
+//Appel de la fonction getcart
 getCart();
 
 /*TOTAL QUANTITY AND PRICE*/
@@ -82,24 +82,53 @@ function getTotal(){
     let quantityProductLenght = quantityProduct.length;
     quantityProductTotal =0;
     //Détermination du nombre d'article pour intégration au total 
-    for (let i = 0; i< quantityProductLenght; ++i){
+    for (let i = 0; i < quantityProductLenght; ++i){
         //valueAsNumber = nbr d'élément interprété comme un nombre
         quantityProductTotal += quantityProduct[i].valueAsNumber;
     }
     //Insertion du nombre de produit dans le HTML
     let quantityCart = document.getElementById("totalQuantity");
     quantityCart.innerHTML = quantityProductTotal;
-    console.log(quantityProductTotal);
+    console.log("quantityProductTotal", quantityProductTotal);
 
     // Price Total
     totalPrice = 0;
     for (var i = 0; i < quantityProductLenght; ++i) {
-        totalPrice += (quantityProduct[i].valueAsNumber * cartArray[i].infoPriceProduct);
+        totalPrice += (quantityProduct[i].valueAsNumber * productInLocalStorage[i].infoPriceProduct);
     }
     //Insertion du prix total dans le HTML
     let productTotalPrice = document.getElementById('totalPrice');
     productTotalPrice.innerHTML = totalPrice;
-    console.log(totalPrice);
+    console.log("totalPrice", totalPrice);
 }
 getTotal();
 
+
+// Modification d'une quantité de produit
+function modifQuantity() {
+    //Récupération des données dans le input
+    let quantityInputs = document.querySelectorAll(".itemQuantity");
+    console.log(quantityInputs)
+
+    for (let i=0; i < quantityInputs.length; i++){
+        //Evenement de modification sur le input
+        quantityInputs[i].addEventListener("change" , (event) => {
+
+            //Récupération de la quantité dans le LS
+            let quantityLS = productInLocalStorage[i].infoQuantityProduct;
+            console.log(quantityLS)
+            
+            //Récupération de la quantité déclarée sur la page panier
+            let quantityModifValue = quantityInputs[i].valueAsNumber;
+            console.log(quantityModifValue)
+            
+            //Modifier la quantité du LS avec la quantité indiquée dans le input
+            productInLocalStorage[i].infoQuantityProduct = quantityModifValue;
+            //Ecraser le LS avec les nouvelles données
+            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+            //Rafraichir la page après chaque clic sur l'input
+            location.reload();
+        })
+    }
+}
+modifQuantity();
