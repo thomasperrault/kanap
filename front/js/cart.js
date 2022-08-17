ab2ef7567a8986a05ca7ac21ad9047108ca57c41
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-08-17 11:11:18 +0200

    Vérification formulaire de contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -36,37 +36,37 @@ function getCart(){
             console.log (infoQuantity);
         }
 
-        //Déclaration de la fonction makecarts pour intégration du code HTML
-        function makeCarts(productInLocalStorage){
-            let html="";
-            for(let i=0; i<productInLocalStorage.length; i++){
-                html = `
-                    <article class="cart__item" data-id="${productInLocalStorage[i].infoIdProduct}" data-color="${productInLocalStorage[i].infoColorProduct}">
-                        <div class="cart__item__img">
-                            <img src=${productInLocalStorage[i].infoImgProduct} alt=${productInLocalStorage[i].infoAltImgProduct}>
-                        </div>
-                        <div class="cart__item__content">
-                            <div class="cart__item__content__description">
-                                <h2>${productInLocalStorage[i].infoNameProduct}</h2>
-                                <p>${productInLocalStorage[i].infoColorProduct}</p>
-                                <p>${productInLocalStorage[i].infoPriceProduct} €</p>
-                            </div>
-                        <div class="cart__item__content__settings">
-                            <div class="cart__item__content__settings__quantity">
-                                <p>Qté :</p>
-                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productInLocalStorage[i].infoQuantityProduct}">
-                            </div>
-                            <div class="cart__item__content__settings__delete">
-                                <p class="deleteItem">Supprimer</p>
-                            </div>
-                        </div>
-                        </div>
-                    </article>
-                `
-            //insertAdjacentHTML insère les noeuds dans la position spécifiée. beforeend = à l'intéreur de l'élément après son dernier enfant
-            cartItems.insertAdjacentHTML("beforeend", html);
-            }
-        }
+//Déclaration de la fonction makecarts pour intégration du code HTML
+function makeCarts(){    
+    let html="";
+    for(let i=0; i<productInLocalStorage.length; i++){
+        html = `
+            <article class="cart__item" data-id="${productInLocalStorage[i].infoIdProduct}" data-color="${productInLocalStorage[i].infoColorProduct}">
+                <div class="cart__item__img">
+                    <img src=${productInLocalStorage[i].infoImgProduct} alt=${productInLocalStorage[i].infoAltImgProduct}>
+                </div>
+                <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>${productInLocalStorage[i].infoNameProduct}</h2>
+                        <p>${productInLocalStorage[i].infoColorProduct}</p>
+                        <p>${productInLocalStorage[i].infoPriceProduct} €</p>
+                    </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Qté :</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productInLocalStorage[i].infoQuantityProduct}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
+                </div>
+                </div>
+            </article>
+        `
+    //insertAdjacentHTML insère les noeuds dans la position spécifiée. beforeend = à l'intéreur de l'élément après son dernier enfant
+    cartItems.insertAdjacentHTML("beforeend", html);
+    }
+}
         //Appel de la fonction makecarts
         makeCarts(productInLocalStorage);
     }
@@ -141,11 +141,7 @@ function deleteProduct(){
         //Evenement de suppression sur le bouton
         btnSupprimer[i].addEventListener("click" , function(){
 
-            //Récupération de la quantité dans le LS
-            let quantityLS = productInLocalStorage[i].infoQuantityProduct;
-            console.log(quantityLS)
-
-            //Modifier la quantité du LS avec la quantité indiquée dans le input
+            //Modifier la quantité du LS à 0
             productInLocalStorage[i].infoQuantityProduct = 0;
             console.log(productInLocalStorage)
 
@@ -162,3 +158,101 @@ function deleteProduct(){
     }
 }
 deleteProduct();
+
+/*VERIFICATION FORMULAIRE*/
+//Vérification du Prénom
+function getFirstNameValidation(){
+    return document.getElementById("firstNameErrorMsg");
+}
+document
+    .getElementById("firstName")
+    .addEventListener("input", function(e) {
+        if (/[a-z,.'-]+$/.test(e.target.value)){
+            getFirstNameValidation().innerText = "";
+        }else{
+            getFirstNameValidation().innerText = "Veuillez renseigner votre prénom"
+            }
+    });
+
+//Vérification du Nom
+function getLastNameValidation(){
+    return document.getElementById("lastNameErrorMsg");
+}
+document
+    .getElementById("lastName")
+    .addEventListener("input", function(e) {
+        if (/[a-z,.'-]+$/.test(e.target.value)){
+            getLastNameValidation().innerText = "";
+        }else{
+            getLastNameValidation().innerText = "Veuillez renseigner votre nom"
+            }
+    });
+
+//Vérification de l'adresse
+function getAddressValidation(){
+    return document.getElementById("addressErrorMsg");
+}
+document
+    .getElementById("address")
+    .addEventListener("input", function(e) {
+        if (/[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+/.test(e.target.value)){
+            getAddressValidation().innerText = "";
+        }else{
+            getAddressValidation().innerText = "Veuillez renseigner votre adresse"
+            }
+    });
+
+//Vérification de la ville
+function getCityValidation(){
+    return document.getElementById("cityErrorMsg");
+}
+document
+    .getElementById("city")
+    .addEventListener("input", function(e) {
+        if (/[a-z,.'-]+$/.test(e.target.value)){
+            getCityValidation().innerText = "";
+        }else{
+            getCityValidation().innerText = "Veuillez renseigner votre ville"
+            }
+    });
+    
+//Vérification de l'email
+function getEmailValidation(){
+    return document.getElementById("emailErrorMsg");
+}
+document
+    .getElementById("email")
+    .addEventListener("input", function(e) {
+        if (/[A-z0-9._-]+[@]{1}[a-zA-Z0-9._-]+[.]{1}[a-zA-Z]{2,10}/.test(e.target.value)){
+            getEmailValidation().innerText = "";
+        }else{
+            getEmailValidation().innerText = "Veuillez renseigner votre email"
+            }
+    });
+    
+    
+
+
+
+/*SAUVEGARDE DONNEES FORMULAIRE
+//Envoi de la commande client au LocalStorage
+function postForm(e) {
+
+    //Ecoute du bouton 'commander'
+    let btnCommander = document.getElementById("order");
+    btnCommander.addEventListener("click", (event)=>{
+
+        //Récupération des produits commandés
+
+        //Récupération des données client
+        let inputFirstName = document.getElementById("firstName");
+        console.log(inputFirstName);
+        let inputLastName = document.getElementById("lastName");
+        let inputAddress = document.getElementById("address");
+        let inputCity = document.getElementById("city");
+        let inputEmail = document.getElementById("email");
+
+    })
+    }
+
+postForm();*/
